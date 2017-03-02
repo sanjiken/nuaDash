213072746b1bbcf3020b8e6cb5d83d4134285495
rouaa <rouaa@rouaa-PC>
2017-03-02 14:41:07 +0100

    RRRR

--- version1/src/main/webapp/js/controller.js
+++ version1/src/main/webapp/js/controller.js
@@ -31,19 +31,42 @@ function CtrlAccueil (angularbeanUser, $scope) {
 		
 		
 	}
-			
+	}}
+	
 	$scope.addUserCtrlJs = function(data){
+		
+{	
+	if (data.userName == null || data.userName== undefined || data.userName  == "" ) {
+			$scope.response == "userName invalid !!!"
+	    }
+	else if (data.email == null || data.email == undefined || data.email  == "" ) {
+	
+		$scope.response == "mail invalid !!!"
+		}
+	else	if (data.password == null || data.password == undefined || data.password  == "" ) {
+		$scope.response == "password invalid !!!"
+		}
+	else if (data.confPassword == null || data.confPassword == undefined || data.confPassword  == "" ) {
+		$scope.response == "confPassword invalide !!!"
+	}
+	
+	else if (data.confPassword != data.password) {
+		$scope.response == "confirmer votre password !!!"
+	} 
+else 
+	 {
+		
 		
 			vm.angularbeanUser.addUserBean(data).then(function(msgUserAdd){
 				("this is the response for adding user : " + msgUserAdd);
-			
+				$scope.response=msgUserAdd;
 			});
 			
-		}
 	
-	
-
-	
-
-
+	 }
 }
+	
+}
+	
+
+
